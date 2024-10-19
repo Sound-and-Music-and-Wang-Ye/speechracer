89d6ece13ac35600cc10fa0cedd4e04dc8938a07
Izz Hafeez <izzhafeez@gmail.com>
2024-10-19 23:17:12 +0800

    Try improve

--- spectrogram/audio_to_img.py
+++ spectrogram/audio_to_img.py
@@ -1,17 +1,22 @@
 import torchaudio
+import torch
 from PIL import Image
 import sys
+import os
+import numpy as np
 
 def audio_to_img(audio_path, img_path, downsample_img_path=None):
 	spectrogram = audio_to_spectrogram(audio_path)
-	downsampled_spectrogram = downsample_spectrogram(spectrogram, 2)
+	spectrogram = torch.mean(spectrogram, dim=0, keepdim=True)
 	spectrogram_to_img(spectrogram, img_path)
 	if downsample_img_path:
-		spectrogram_to_img(downsampled_spectrogram, downsample_img_path)
+		pass
+		# downsampled_spectrogram = downsample_spectrogram(spectrogram, 2)
+		# spectrogram_to_img(downsampled_spectrogram, downsample_img_path)
 
 def audio_to_spectrogram(audio_path):
 	audio, sr = torchaudio.load(audio_path)
-	spectrogram = torchaudio.transforms.MelSpectrogram()(audio)
+	spectrogram = torchaudio.transforms.MelSpectrogram(n_fft=2048, n_mels=256, f_min=50, f_max=8000)(audio)
 	return spectrogram
 
 def downsample_spectrogram(spectrogram, downsample_factor):
@@ -22,7 +27,10 @@ def downsample_spectrogram(spectrogram, downsample_factor):
 
 def spectrogram_to_img(spectrogram, img_path):
 	spectrogram = spectrogram.squeeze(0).numpy()
-	img = Image.fromarray(spectrogram)
+	# log 10 of the spectrogram
+	scaled_spectrum = np.log10(spectrogram + 1)
+	print(scaled_spectrum)
+	img = Image.fromarray(scaled_spectrum)
 	img.save(img_path)
 
 if __name__ == '__main__':

02c23b99f5b0dfe4901517f04e754de36230eed8
shotnothing <shotnothing@protonmail.ch>
2024-10-23 23:15:53 +0800

    Add start and end props to TextDisplay

--- app/src/components/TextDisplay.jsx
+++ app/src/components/TextDisplay.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react';
 import { Box, Flex, Text } from '@chakra-ui/react';
 
-const TextDisplay = ({ words, progress }) => {
+const TextDisplay = ({ 
+    words, 
+    progress,
+    start=0,
+    end=words.length,
+}) => {
     return (
         <Box
             display="flex"
             flexWrap="wrap"
             w="100%"
         >
-            {words.map((word, index) => (
+            {words.slice(start, end).map((word, index) => (
                 <Text key={index} fontSize="5xl" mr={2} mb={8}
-                    color={index < progress ? "green.400" : "gray.400"}
+                    color={index < progress-start ? "green.400" : "gray.400"}
                 >
                     {word}
                 </Text>

45869a58e3b8a0fa1e08ba1ce29da10be075debd
Izz Hafeez <izzhafeez@gmail.com>
2024-10-24 00:37:21 +0800

    Change to player progresses

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,14 @@
 import asyncio
+import random
 from datetime import datetime
 from typing import List, Dict, Any
-from pymongo import MongoClient
-from fastapi import FastAPI, Depends, WebSocket
+from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.websockets import WebSocketDisconnect
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from typing_extensions import Annotated
+from pydantic_settings import BaseSettings
 from functools import lru_cache
+
+PARA_COUNT = 6343
 
 class Settings(BaseSettings):
     mongo_password: str
@@ -28,22 +28,19 @@ app.add_middleware(
 
 # 1 minute lobby, 4 minutes game time
 class GameInstance:
-    def __init__(self, text: str, time_entered: datetime):
+    def __init__(self, para_id: int, time_entered: datetime):
         self.players: Dict[str, WebSocket] = {}
-        self.text = text
-        self.text_length = len(text)
+        self.player_progresses: Dict[str, int] = {}
+        self.para_id = para_id
         self.time_entered = time_entered
         asyncio.create_task(self.start_game())
-
-    def get_player_data(self):
-        return {name: {"x": 0, "y": 0} for name in self.players.keys()}
 
     async def handle_connection(self, websocket: WebSocket, name: str):
         self.players[name] = websocket
 
         await websocket.send_json({
             "method": "connect",
-            "players": self.get_player_data()
+            "players": self.player_progresses
         })
 
     async def handle_client(self, websocket: WebSocket):
@@ -70,20 +67,20 @@ class GameInstance:
         for player in self.players.values():
             await player.send_json({
                 "method": method,
-                "get_player_data": self.get_player_data(),
+                "player_progresses": self.player_progresses,
                 **data
             })
         
     async def start_game(self):
         await asyncio.sleep(60)
-        await self.notify_all_players("start", {"text": self.text})
+        await self.notify_all_players("start", {"para_id": self.para_id})
         await asyncio.sleep(240)
         await self.notify_all_players("end", {})
 
 game_instances: Dict[str, GameInstance] = {}
 
 @app.websocket("/ws/lobby/{name}")
-async def websocket_endpoint(websocket: WebSocket, name: str, settings: Annotated[Settings, Depends(get_settings)]):
+async def websocket_endpoint(websocket: WebSocket, name: str):
     await websocket.accept()
 
     time_entered = datetime.now()
@@ -91,10 +88,8 @@ async def websocket_endpoint(websocket: WebSocket, name: str, settings: Annotate
     is_new_game = time_entered_key not in game_instances
 
     if is_new_game:
-        connection = f"mongodb+srv://admin:{settings.mongo_password}@cluster0.1jxisbd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsCAFile=isrgrootx1.pem"
-        client = MongoClient(connection)
-        random_text = await client.speechracer.texts.find_one()
-        game_instances[time_entered_key] = GameInstance(random_text, time_entered)
+        para_id = random.randint(0, PARA_COUNT)
+        game_instances[time_entered_key] = GameInstance(para_id, time_entered)
     
     game_instance = game_instances[time_entered_key]
     await game_instance.handle_connection(websocket, name)
